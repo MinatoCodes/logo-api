acdff4357e3a963bd07be548727bab71e461f809
MinatoCodes <subinsirmal11@gmail.com>
2025-07-26 22:34:19 +0545

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,5 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ status: 'ok', message: 'API is alive 💓' });
-}
+module.exports = (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    message: 'API is healthy 🚀'
+  });
+};
